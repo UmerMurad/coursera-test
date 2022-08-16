f847ad74e5221dca4f09db03fab57a671d0162bb
Farhina Kiran <farhinakiran@Jawads-MacBook-Pro-2.local>
2022-08-16 14:16:27 +0500

    addition

---
+++ ajax/js/script.js
@@ -0,0 +1,23 @@
+// Event Handling
+document.addEventListener("DOMContentLoaded",
+	function (event) {
+
+		// Unobstrusive event binding
+		document.querySelector("button")
+			.addEventListener("click", function () {
+				var self = this;
+				var name = "";
+
+				// call server to get the name
+				$ajaxUtils
+					.sendGetRequest("/data/name.text", 
+						function(request) {
+							var name = request.responseText;
+							document.querySelector("#content")
+								.innerHTML = "<h2>Hello " + name + "!";
+
+						});
+				
+			});
+	}
+);
