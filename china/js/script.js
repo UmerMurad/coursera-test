c7f4e31b0688d0c66eb96f3f5748723722ab0da2
Farhina Kiran <farhinakiran@Jawads-MacBook-Pro-2.local>
2022-08-19 19:45:49 +0500

    new

--- china/js/script.js
+++ china/js/script.js
@@ -21,6 +21,11 @@ $(function () { //same as document.addEventListener("DOMContentLoaded")
 	var categoriesTitleHtml = "snippets/categories-title-snippet.html";
 	var categoryHtml = "snippets/category-snippet.html";
 
+	var menuItemsUrl =
+		"http://davids-restaurant.herokuapp.com/menu_items.json?category=";
+	var menuItemsTitleHtml = "snippets/menu-items-title-.html";
+	var menuItemHtml = "snippets/menu-item.html";
+
 
 
 	// Convinience function for inserting innerHTML for "selector"
@@ -70,6 +75,15 @@ $(function () { //same as document.addEventListener("DOMContentLoaded")
 			buildAndShowCategoriesHTML);
 	};
 
+	// Load the menu items view
+	//  'categoryShort' is a short_name for a categgory
+	dc.loaadMenuItems = function (categoryShort) {
+		showLoading("#main-content");
+		$ajaxUtils.sendGetRequest(
+			menuItemsUrl + categoryShort,
+			buildAndShowMenuItemsHTML);
+	};
+
 	// Builds HTML for the categories page based on the data 
 	// from the server
 	function buildAndShowCategoriesHTML (categories) {
@@ -84,7 +98,7 @@ $(function () { //same as document.addEventListener("DOMContentLoaded")
 						var categoriesViewHtml =
 							buildCategoriesViewHtml(categories,
 								categoriesTitleHtml,
-								categoryHtml):
+								categoryHtml);
 						insertHtml("main-content", categoriesViewHtml);
 					},
 					false);
@@ -119,6 +133,87 @@ $(function () { //same as document.addEventListener("DOMContentLoaded")
 		return finalHtml;
 	}
 
+	// Builds HTML for the single category page based on the data
+	// from the server 
+	function buildAndShowMenuItemsHTML (categoryMenuItems) {
+		// Load title snippet of menu items page
+		$ajaxUtils.sendGetRequest(
+			menuItemsTitleHtml,
+			function (menuItemsTitleHtml) {
+				// Retrieve single menu item snippet
+				$ajaxUtils.sendGetRequest(
+					menuItemHtml,
+					function (menuItemHtml) {
+						var menuItemsViewHtml = 
+							buildMenuItemsViewHtml(categoryMenuItems,
+								menuItemsTitleHtml, menuItemHtml);
+						insertHtml("#main-content", menuItemsViewHtml);
+					},
+					false);
+			},
+			false);
+	}
+
+
+
+	// Using categories data and snippets html
+	// build menu items view HTML to be inserted into page
+
+	function buildMenuItemsViewHtml(categoryMenuItems,
+								menuItemsTitleHtml,
+								menuItemHtml) {
+
+		menuItemsTitleHtml = 
+			insertProperty(menuItemsTitleHtml,
+							"name",
+							categoryMenuItems.category.name);
+		menuItemsTitleHtml = 
+			insertProperty(menuItemsTitleHtml,
+							"special_instructions",
+							categoryMenuItems.category.special_instructions);
+		var finalHtml = menuItemsTitleHtml;
+		finalHtml += "<section class='row'>";
+
+		//Loop over categories
+		var menuItems = categoryMenuItems.menu_items;
+		var categoryShortName = categoryMenuItems.category.short_name;
+
+		for (var i = 0; i <menuItems.length; i++) {
+			// Insert menu items values
+			var html = menuItemHtml;
+			
+			html = 
+				insertProperty(html, "shirt_name", menuItems[i].short_name);
+			html =
+				insertProperty(html, "catShortName", catShortName);
+			finalHtml += html;
+			html = 
+				insertItemPrice(html, "price_small", menuItems[i].price_small);
+
+			html = 
+				insertItemPortionName(html, "small_portion_name",
+										menuItems[i].small_portion_name);
+
+			html = 
+				insertItemPrice(html, "price_large", menuItems[i].price_large);
+
+			html = 
+				insertItemPortionName(html, "large_portion_name",
+										menuItems[i].large_portion_name);
+			html = 
+				insertProperty(html, "name", menuItems[i].name);
+		html = 
+				insertProperty(html, "description", menuItems[i].description);
+
+		if (i % 2 != 0) {
+			html += 
+			"<div class='clearfix visible-lg-block visible-md-block'></div>"
+		}
+
+		}
+		finalHtml += "</section>";
+		return finalHtml;
+	}
 
 
 
